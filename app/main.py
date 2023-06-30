0b0ba59f9c48bf446fbffab37c20323638d16298
randy <rtang0203@gmail.com>
2023-06-29 20:32:36 -0500

    adding login and db functionality

--- app/main.py
+++ app/main.py
@@ -1,34 +1,55 @@
 from flask import Blueprint
+from flask_login import login_required, current_user
+from .models import ReadingLog
+from datetime import date
 from . import db
 
 main = Blueprint('main', __name__)
 
 from flask import Flask, render_template, request, url_for, flash, redirect, Blueprint
 
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
-            ]
+def hasReadToday():
+    if current_user.is_authenticated:
+        todaysEntry = ReadingLog.query.filter_by(user_id=current_user.id, date=date.today()).first()
+        if todaysEntry:
+            return True
+    return False
 
-readToday = False
-
-@main.route('/', methods = ('GET', 'POST'))
+@main.route('/', methods = ['GET'])
 def index():
-    if request.method == 'POST':
-        global readToday
-        readToday = True
-    #return render_template('index.html', messages = messages)
-    return render_template('index.html', readToday = readToday)
+    if current_user.is_authenticated:
+        return render_template('index.html', readToday = hasReadToday(), name=current_user.name)
+    return render_template('index.html', readToday = False)
+
+@main.route('/', methods = ['POST'])
+@login_required
+def index_post():
+    userId = current_user.id 
+    currentDate = date.today()
+
+    # Create a new ReadingLog entry
+    entry = ReadingLog(user_id=userId, date=currentDate)
+
+    # Add the entry to the database
+    db.session.add(entry)
+    db.session.commit()
+
+    return redirect(url_for('main.index'))
 
 @main.route('/reset', methods = ['POST'])
+@login_required
 def reset():
     if request.method == 'POST':
-        global readToday
-        readToday = False
+        # global readToday
+        # readToday = False
+        if current_user.is_authenticated:
+            todaysEntry = ReadingLog.query.filter_by(user_id=current_user.id, date=date.today()).first()
+            if todaysEntry:
+                db.session.delete(todaysEntry)
+                db.session.commit()
     return redirect(url_for('main.index'))
-    #return render_template('index.html', readToday = readToday)
 
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', name=current_user.name)
---
+++ app/models.py
@@ -0,0 +1,26 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column
+
+class ReadingLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date)
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_name = db.Column(db.String(100))
+
+class GroupMember(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
